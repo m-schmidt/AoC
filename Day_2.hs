94087c0f42ccbe5d089d8547d01d495066241366
Michael Schmidt <github@mschmidt.me>
2017-12-19 00:19:30 +0100

    Add part2 for day 2

--- Day_2.hs
+++ Day_2.hs
@@ -5,6 +5,8 @@ import Control.Applicative
 {-
   http://adventofcode.com/2017/day/2
 
+  Part 1:
+
   As you walk through the door, a glowing humanoid shape yells in
   your direction. "You there! Your state appears to be idle. Come
   help us repair the corruption in this spreadsheet - if we take
@@ -34,6 +36,31 @@ import Control.Applicative
   In this example, the spreadsheet's checksum would be:
 
     8 + 4 + 6 = 18.
+
+  Part 2:
+
+  [...]
+
+  It sounds like the goal is to find the only two numbers in each
+  row where one evenly divides the other - that is, where the
+  result of the division operation is a whole number. They would
+  like you to find those numbers on each line, divide them, and add
+  up each line's result.
+
+  For example, given the following spreadsheet:
+
+    5 9 2 8
+    9 4 7 3
+    3 8 6 5
+
+  - In the first row, the only two numbers that evenly divide are 8
+    and 2; the result of this division is 4.
+
+  - In the second row, the two numbers are 9 and 3; the result is 3.
+
+  - In the third row, the result is 2.
+
+  In this example, the sum of the results would be 4 + 3 + 2 = 9.
 -}
 
 
@@ -42,15 +69,25 @@ convert :: String -> [[Int]]
 convert = map (map read . words) . filter (not . null) . lines
 
 -- Sums the differences of all rows of a matrix
-checkSum :: [[Int]] -> Int
-checkSum = sum . map diff
+checkSum1 :: [[Int]] -> Int
+checkSum1 = sum . map diff
   where
     diff = liftA2 (-) maximum minimum
 
+-- Sums the quotients of the evenly dividable numbers
+checkSum2 :: [[Int]] -> Int
+checkSum2 = sum . map even_div
+  where
+    even_div xs = head [ x `div` y | x <- xs, y <- xs, x /= y, x `mod` y == 0]
+
 
 main = do
-  putStrLn $ solve "5 1 9 5\n\
-                   \7 5 3\n\
-                   \2 4 6 8"
+  solve checkSum1 "5 1 9 5\n\
+                  \7 5 3\n\
+                  \2 4 6 8"
+
+  solve checkSum2 "5 9 2 8\n\
+                  \9 4 7 3\n\
+                  \3 8 6 5"
   where
-    solve x = concat [ x, " => ", show . checkSum . convert $ x ]
+    solve f x = putStrLn $ concat [ x, " => ", show . f . convert $ x ]
