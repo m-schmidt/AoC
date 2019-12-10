397b5626e5c0e556e5b0742768874a7430b1af3c
Michael Schmidt <github@mschmidt.me>
2019-12-10 12:48:54 +0100

    Extract recursion schemes into separate module

--- 2019/day_6.hs
+++ 2019/day_6.hs
@@ -2,25 +2,10 @@
 
 -- https://adventofcode.com/2019/day/6
 
+
 import qualified Data.Map.Strict as Map
 import Data.Maybe
-
-
-
--- Recursion schemes
-newtype Fix f = Fix { unFix :: f (Fix f) }
-
-type Coalgebra f a = a -> f a
-type Algebra f a = f a -> a
-
-ana :: Functor f => Coalgebra f a -> a -> Fix f
-ana coalg = Fix . fmap (ana coalg) . coalg
-
-cata :: Functor f => Algebra f a -> Fix f -> a
-cata alg = alg . fmap (cata alg) . unFix
-
-hylo :: Functor f => Algebra f a -> Coalgebra f b -> b -> a
-hylo f g = f . fmap (hylo f g) . g
+import Recursion
 
 
 -- OrbitData maps each object to the list of objectis directly orbiting it
